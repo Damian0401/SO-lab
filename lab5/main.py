1be25b9f728fb1dcb33d65946c84744dbd72261e
kAleks12 <kacper4553@hotmail.com>
2023-05-22 16:26:57 +0200

    Add outcome announcing and game over detection

--- lab5/main.py
+++ lab5/main.py
@@ -1,4 +1,4 @@
-from studentA import print_board, is_game_over, new_board
+from studentA import print_board, is_game_over, new_board, announce_outcome
 from studentB import ai_move, get_user_move, is_player_starting
 
 board = new_board(3)
@@ -8,3 +8,6 @@ while not is_game_over(board):
     print_board(board)
     board = get_user_move(board) if user_turn else ai_move(board)
     user_turn = not user_turn
+
+announce_outcome(board)
+
--- lab5/studentA.py
+++ lab5/studentA.py
@@ -20,21 +20,129 @@ def new_board(size=3):
     return board
 
 
-def is_game_over(board) -> bool:
-    for row in board:
-        for element in row:
-            if element == Move.EMPTY:
-                return False
+def is_game_over(board) -> (bool, Move):
+    output = _check_horizontal(board)
+    if output != Move.EMPTY:
+        return True
+
+    output = _check_vertical(board)
+    if output != Move.EMPTY:
+        return True
+
+    output = _check_diagonal(board)
+    if output != Move.EMPTY:
+        return True
+
+    return _check_board_full(board)
+
+
+def announce_outcome(board):
+    output = _check_diagonal(board)
+    if output == Move.X:
+        print("X's won!")
+    elif output == Move.O:
+        print("0's won!")
+
+    output = _check_vertical(board)
+    if output == Move.X:
+        print("X's won!")
+    elif output == Move.O:
+        print("0's won!")
+
+    output = _check_horizontal(board)
+    if output == Move.X:
+        print("X's won!")
+    elif output == Move.O:
+        print("0's won!")
+
+    print("Game ended with a draw!")
 
 
 def print_board(board: List[List[Move]]):
     for row in board:
         for element in row:
             if element == Move.EMPTY:
-                print("-", end="")
+                print("| - |", end="")
             elif element == Move.X:
-                print("X", end="")
+                print("| X |", end="")
             else:
-                print("O", end="")
+                print("| O |", end="")
 
         print()
+
+    print("\n\n")
+
+
+def _check_board_full(board) -> bool:
+    for row in board:
+        for element in row:
+            if element == Move.EMPTY:
+                return False
+
+    return True
+
+
+def _check_horizontal(board) -> Move:
+    counter_x = 0
+    counter_o = 0
+
+    for row in board:
+        for element in row:
+            if element == Move.X:
+                counter_x += 1
+            elif element == Move.O:
+                counter_o += 1
+
+        if counter_x == 3:
+            return Move.X
+        else:
+            counter_x = 0
+
+        if counter_o == 3:
+            return Move.O
+        else:
+            counter_o = 0
+
+    return Move.EMPTY
+
+
+def _check_vertical(board) -> Move:
+    counter_x = 0
+    counter_o = 0
+
+    for i in range(len(board)):
+        for j in range(board):
+
+            if board[j][i] == Move.X:
+                counter_x += 1
+
+            elif board[j][i] == Move.O:
+                counter_o += 1
+
+        if counter_x == 3:
+            return Move.X
+        else:
+            counter_x = 0
+
+        if counter_o == 3:
+            return Move.O
+        else:
+            counter_o = 0
+
+    return Move.EMPTY
+
+
+def _check_diagonal(board) -> Move:
+    if board[0][0] == Move.X and board[1][1] == Move.X and board[2][2] == Move.X:
+        return Move.X
+
+    if board[0][0] == Move.O and board[1][1] == Move.O and board[2][2] == Move.O:
+        return Move.O
+
+    if board[0][2] == Move.X and board[1][1] == Move.X and board[2][0] == Move.X:
+        return Move.X
+
+    if board[0][2] == Move.O and board[1][1] == Move.O and board[2][0] == Move.O:
+        return Move.O
+
+    return Move.EMPTY
