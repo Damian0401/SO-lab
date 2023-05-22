8710461e44635e99ab1b810c64d0a6c1892aad94
Damian0401 <szkola_abc@o2.pl>
2023-05-22 15:53:13 +0200

    Added get_user_move function

--- lab5/studentB.py
+++ lab5/studentB.py
@@ -1,13 +1,34 @@
 
 from random import Random
 
+from studentA import Move
+
 random = Random()
 
 def ai_move(board):
     pass
 
 def get_user_move(board):
-    pass
+    available_moves = []
+
+    for i in range(0, len(board)):
+        for j in range(0, len(board)):
+            if board[i][j] == Move.EMPTY:
+                available_moves.append((i, j))
+    
+    while True:
+        print("Enter your move:")
+        print("x: ")
+        x = int(input())
+        print("y: ")
+        y = int(input())
+
+        if (x, y) in available_moves:
+            board[x][y] = Move.X
+            return board
+        else:
+            print("Invalid move. Try again.")
+    
 
 def is_player_starting():
     print("Head or tails? (h/t)")
